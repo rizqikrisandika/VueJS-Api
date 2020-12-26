a6fd0c50aae13277efafa7caa4ac2f6813364cbf
rizqikrisandika <rizqikrisandika990@gmail.com>
2020-12-26 15:49:26 +0700

    add nav

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+     <router-link class="navbar-brand" to="/">Kuliner</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/foods">Foods</router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/keranjang">Keranjang
+            <b-icon-bag></b-icon-bag>
+            <span class="badge badge-success ml-2">0</span>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style>
+</style>
--- src/views/Foods.vue
+++ src/views/Foods.vue
@@ -1,22 +1,20 @@
 <template>
-  <div class="home">
+  <div>
     <Navbar />
-    <div class="container">
-      <Hero />
-    </div>
+    <h2>Daftar Makanan</h2>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
-import Hero from "@/components/Hero.vue";
 
 export default {
-  name: "Home",
+  name: "Foods",
   components: {
     Navbar,
-    Hero
   },
 };
 </script>
+
+<style>
+</style>
